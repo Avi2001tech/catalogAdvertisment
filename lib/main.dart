1aff054d71370e7cdb07404f3ec18eda8ac5445c
Abhishek Kumar Shaw <avitech2001@gmail.com>
2023-08-01 11:10:34 +0530

    basic acrivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:catalog_new/pages/home_page.dart';
+import 'package:catalog_new/pages/login_page.dart';
+import 'package:catalog_new/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,17 +12,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("welcome to house new"),
-          ),
-        ),
-      ),
+      //home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.green,
+          fontFamily: GoogleFonts.lato().fontFamily),
+
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.green),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: avoid_print
+
+import 'package:catalog_new/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 19.0),
+              child: Image.asset(
+                "assets/images/login_yellow.png",
+                height: 250,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              child: Text("Login"),
+              style: TextButton.styleFrom(),
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
